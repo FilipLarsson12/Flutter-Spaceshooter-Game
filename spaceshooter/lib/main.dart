5284d1d936dba2078233f97cc6cb0590ebb2ea3f
FilipLarsson12 <116650611+FilipLarsson12@users.noreply.github.com>
2023-03-25 14:54:15 +0100

    Update main.dart

--- spaceshooter/lib/main.dart
+++ spaceshooter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: Scaffold(
         body: Container(
-            decoration: BoxDecoration(color: Colors.red),
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(color: Colors.white),
             child: CustomPaint(
               painter: spaceShuttle(
                   leftpoint: Offset(50, 300),
@@ -26,25 +29,78 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class spaceShooterGame extends StatefulWidget {
+
+  @override
+  spaceShooterGameState createState() => spaceShooterGameState();
+}
+
 class spaceShuttle extends CustomPainter {
   final Offset leftpoint;
   final Offset topPoint;
   final Offset rightPoint;
+  final double gunWidth = 10;
+  final double gunheight = 20;
+  final List<Offset> leftGunCoordinates;
   spaceShuttle(
       {required this.leftpoint,
       required this.topPoint,
-      required this.rightPoint});
+      required this.rightPoint,
+      
+      });
+
+  void drawLeftGun(Path path) {
+    path.moveTo(leftpoint.dx + (topPoint.dx - leftpoint.dx) / 2,
+        leftpoint.dy - (leftpoint.dy - topPoint.dy) / 2);
+    path.lineTo(x, y)
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
     final Paint paint = Paint();
     paint.color = Colors.blue;
-    paint
 
-    canvas.drawLine(leftpoint, topPoint, paint);
-    canvas.drawLine(topPoint, rightPoint, paint);
-    canvas.drawLine(rightPoint, leftpoint, paint);
+    Path path = Path();
+    path.moveTo(leftpoint.dx, leftpoint.dy);
+    path.lineTo(topPoint.dx, topPoint.dy);
+    path.lineTo(rightPoint.dx, rightPoint.dy);
+    path.close();
+    canvas.drawPath(path, paint);
   }
 
   bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
+
+class CirclePainter extends CustomPainter {
+  final Color borderColor;
+  final Color fillColor;
+  final double borderWidth;
+
+  CirclePainter({
+    required this.borderColor,
+    required this.fillColor,
+    required this.borderWidth,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = min(size.width / 2, size.height / 2) - borderWidth / 2;
+
+    final borderPaint = Paint()
+      ..color = borderColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = borderWidth;
+
+    final fillPaint = Paint()..color = fillColor;
+
+    canvas.drawCircle(center, radius, borderPaint);
+    canvas.drawCircle(center, radius - borderWidth / 2, fillPaint);
+  }
+
+  @override
+  bool shouldRepaint(CirclePainter oldDelegate) =>
+      borderColor != oldDelegate.borderColor ||
+      fillColor != oldDelegate.fillColor ||
+      borderWidth != oldDelegate.borderWidth;
 }
